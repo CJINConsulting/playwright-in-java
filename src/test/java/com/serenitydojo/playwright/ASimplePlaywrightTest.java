637d33b4a7ec3617aea68d2119a0f175091f5bf7
Colin Piper <colin.piper@viperinnovations.com>
2024-12-01 13:22:06 +0000

    Lesson 2

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -3,13 +3,30 @@ package com.serenitydojo.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Assertions;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@UsePlaywright
 public class ASimplePlaywrightTest {
+    @Test
+    void shouldShowThePageTitle(Page page) {
+        page.navigate("https://practicesoftwaretesting.com");
+        String title = page.title();
+        assertThat(title).contains("Practice Software Testing");
+    }
 
     @Test
-    void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
+    void shouldSearchByKeyword(Page page) {
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder='Search']").fill("pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearch = page.locator(".card").count();
+        assertTrue(matchingSearch > 0);
     }
 }
